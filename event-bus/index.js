9b09f1b84202982087c19a398fdcea3a78e49eef
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-09-24 02:23:22 +0500

    Test Post and Event-Bus services with addition of cluster-ip services method in both deployment file

--- event-bus/index.js
+++ event-bus/index.js
@@ -11,19 +11,21 @@ app.post("/events", (req, res) => {
   const event = req.body;
 
   events.push(event);
+console.log('before')
+  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
 
-  axios.post("http://localhost:4000/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4001/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://localhost:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
+  console.log('after')
+  // axios.post("http://localhost:4001/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4002/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
+  // axios.post("http://localhost:4003/events", event).catch((err) => {
+  //   console.log(err.message);
+  // });
   res.send({ status: "OK" });
 });
 
