d5438548a27f7331dbd2795e85159b5f7492d179
muhammad bilal <muhammadbilal@muhammads-MacBook-Pro.local>
2022-09-24 15:30:22 +0500

    Chnage url to service name in event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,16 +16,17 @@ console.log('before')
     console.log(err.message);
   });
 
-  console.log('after')
-  // axios.post("http://localhost:4001/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4002/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
-  // axios.post("http://localhost:4003/events", event).catch((err) => {
-  //   console.log(err.message);
-  // });
+ 
+  axios.post("http://comments-srv:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://query-srv:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://moderation-src:4003/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  console.log('afterr ')
   res.send({ status: "OK" });
 });
 
